27512e60d6c14931579c22d263f31c089ce6650b
Luiza Mendes Fonseca Ferreira Neta <luizamendes@sefaz.pi.gov.br>
2021-07-23 10:35:28 -0300

    adicionando Swagger

--- cadastroDeTarefa/src/main/java/sefaz/pi/gov/br/cadastroDeTarefa/CadastroDeTarefaApplication.java
+++ cadastroDeTarefa/src/main/java/sefaz/pi/gov/br/cadastroDeTarefa/CadastroDeTarefaApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableSpringDataWebSupport //para permitir a paginacao com o Pageable
+@EnableSwagger2 // para habilitar o swwager
 public class CadastroDeTarefaApplication {
 
 	public static void main(String[] args) {
